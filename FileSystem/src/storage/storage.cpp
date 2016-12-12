22649b762d3cfac8a13aa2445779a4e1595643fd
Evan Thompson <evanmt13@gmail.com>
2016-12-12 17:57:57 -0500

    Updated to check if storage directory already exists

--- FileSystem/src/storage/storage.cpp
+++ FileSystem/src/storage/storage.cpp
@@ -1,8 +1,29 @@
 // Storage server for Zing
 #include "classes/FileSystem/FileSystem.h"
+#include <unistd.h>
+#include <iostream>
+#include <string.h>
+#include <sys/stat.h>
+
+#define STORAGE_DIR "/zingStore"
+
+// create storage directory if it doesn't exist
+void setup(){
+    struct stat store;
+    char* path = (char*)malloc(256 * sizeof(char));
+    path[0] = '\0';
+    getcwd(path, 256);
+    strcat(path, STORAGE_DIR);
+    if(stat(path, &store) == 0 && S_ISDIR(store.st_mode)){
+        std::cout << " Storage already exists at path: " << path << std::endl;
+    }else{
+        std::cout << " Does not exist " << std::endl;
+    }
+    
+}
 
 int main(int args, char** argc){
 
-    
+    setup();    
     
 }
