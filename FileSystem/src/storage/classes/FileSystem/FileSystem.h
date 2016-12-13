31b8a433aad3ae08771479d1d7e575212a1b646f
Evan Thompson <evanmt13@gmail.com>
2016-12-12 23:44:24 -0500

    Added update to crawl

--- FileSystem/src/storage/classes/FileSystem/FileSystem.h
+++ FileSystem/src/storage/classes/FileSystem/FileSystem.h
@@ -1,8 +1,14 @@
 #include <unordered_map>
+#include <vector>
+
+#define FS_SAVE_FILE "fssave.sv"
 
 class FileSystem { 
     public:
         FileSystem();
         void crawl(char* path, int size);
+
+    private:
+        //std::unordered_map<std::string, int> hashToID;
 
 };
